432e69d5606282bf574cb2a0e55c36d90bba8285
Himefe <idanger2015@hotmail.com>
2021-12-26 19:06:16 -0300

    Adicionando todos os arquivos no repositório

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Contato from "./components/Contato";
+import Header from "./components/Header";
+import Produtos from "./components/Produtos";
+import Product from "./components/Product";
+import "./index.css";
+
+// Utilize a API abaixo para puxar a lista de produto
+// https://ranekapi.origamid.dev/json/api/produto
+// Cada produto possui o id, o mesmo pode ser passado na api para retornar os dados desse produto específico
+// https://ranekapi.origamid.dev/json/api/produto/notebook
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <BrowserRouter>
+        <Header />
+        <div>
+          <Routes>
+            <Route path="/" element={<Produtos />} />
+            <Route path="/contato" element={<Contato />} />
+            <Route path="produto/:id" element={<Product />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
